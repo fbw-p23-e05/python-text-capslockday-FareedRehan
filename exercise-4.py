914b592761b7a78c29ad2f2e6a76d84b9144eb81
FareedRehan <fareed.rehan@dci-student.org>
2023-08-31 20:49:01 +0200

    exercise-3.py is added

---
+++ exercise-4.py
@@ -0,0 +1,15 @@
+# CAPS LOCK DAY is over!
+
+## Tasks
+
+### Input --> Output 1
+text = str(input("Enter 'CAPS LOCK DAY IS OVER': ", ))
+print(text.capitalize() + ("!"))
+
+### Input --> Output 2
+text = str(input("Enter 'Today is not caps lock day': ", ))
+print(text.capitalize() + ".")
+
+### Input --> Output 3
+text = str(input("Enter 'Let us stay calm, no need to panic': ", ))
+print(text.capitalize() + ".")
